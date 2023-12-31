5a0824536d65d6fa1868049a8b731009373ca3ad
kazukiUshijima <info@kfs214.net>
2023-12-31 15:37:00 +0900

    feat: dynamically colored funchan

--- src/features/Home/DynamicFunchan/DynamicFunchan.tsx
+++ src/features/Home/DynamicFunchan/DynamicFunchan.tsx
@@ -1,8 +1,19 @@
+import { styled } from '@mui/material/styles'
+
+import FunchanTemplate from '@/assets/funchan-template.svg'
+
 type Props = {
   content: string
   primaryColor: string
   secondaryColor: string
 }
+
+const StyledFunchanTemplate = styled(FunchanTemplate)<{
+  fill: string
+  stroke: string
+}>(({ fill, stroke }) => ({
+  path: { fill: `#${fill}`, stroke: `#${stroke}` },
+}))
 
 export function DynamicFunchan({
   content,
@@ -10,8 +21,9 @@ export function DynamicFunchan({
   secondaryColor,
 }: Props) {
   return (
-    <div>
-      {content}, {primaryColor}, {secondaryColor}
-    </div>
+    <StyledFunchanTemplate
+      fill={primaryColor || '094f6a'}
+      stroke={secondaryColor || 'ffffff'}
+    />
   )
 }
